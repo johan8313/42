e75951ed95a0409635c55bc636abb163b4d5fd76
johan8313 <johanmartinez@msn.com>
2024-11-17 13:40:29 +0100

    Create server.c

---
+++ server.c
@@ -0,0 +1,30 @@
+#include "minitalk.h"
+
+void	handler(int sig)
+{
+	static int				i = 0;
+	static unsigned char	c = 0;
+
+	if (sig == SIGUSR2)
+		c = c << 1;
+	else if (sig == SIGUSR1)
+		c = (c << 1) | 0b00000001;
+	i++;
+	if (i == 8)
+	{
+		ft_printf("%c", c);
+		i = 0;
+		c = 0;
+	}
+}
+
+int	main(void)
+{
+	ft_printf("PID: %d\n", getpid());
+	while (1)
+	{
+		signal(SIGUSR1, handler);
+		signal(SIGUSR2, handler);
+	}
+	return (0);
+}
