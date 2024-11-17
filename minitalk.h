d257a8a1e2d28622047c073b689aebe801925250
johan8313 <johanmartinez@msn.com>
2024-11-17 13:41:56 +0100

    Create minitalk.h

---
+++ minitalk.h
@@ -0,0 +1,11 @@
+#ifndef MINITALK_H
+# define MINITALK_H
+
+# include <bits/types/siginfo_t.h>
+# include <signal.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include "libft/libft.h"
+# include "libft/ft_printf/ft_printf.h"
+
+#endif
